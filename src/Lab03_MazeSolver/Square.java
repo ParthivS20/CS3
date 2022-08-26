d16ca92f74ea89eb06875ac3fc79cb525b8fe1dc
Parthiv Shah <parthivjs12@gmail.com>
2022-08-26 14:13:38 -0500

    Control updates

---
+++ src/Lab03_MazeSolver/Square.java
@@ -0,0 +1,21 @@
+package Lab03_MazeSolver;
+
+public class Square {
+    public static final int EMPTY = 0;
+    public static final int WALL = 1;
+    public static final int START = 2;
+    public static final int EXIT = 3;
+
+    private int type;
+    private int row;
+    private int col;
+
+    public Square(int row, int col, int type) {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
