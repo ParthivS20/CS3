c37bf56fec369465a4c3c89b1aae667af08c186e
Parthiv Shah <parthivjs12@gmail.com>
2022-09-21 16:27:59 -0500

    finished maze solver

--- src/Lab04_MelodyMaker/QueueProbs.java
+++ src/Lab04_MelodyMaker/QueueProbs.java
@@ -2,9 +2,67 @@ package Lab04_MelodyMaker;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class QueueProbs {
     Queue<Integer> evenFirst(Queue<Integer> nums) {
-        return new LinkedList<>();
+        Queue<Integer> even = new LinkedList<>();
+        Queue<Integer> odd = new LinkedList<>();
+        int initSize = nums.size();
+
+        for(int i = 0; i < initSize; i++) {
+            if(nums.peek() % 2 == 0) {
+                even.offer(nums.poll());
+            }
+            else {
+                odd.offer(nums.poll());
+            }
+        }
+
+        while(!odd.isEmpty()) even.offer(odd.poll());
+
+        return even;
+    }
+
+    boolean numPalindrome(Queue<Integer> nums) {
+        Queue<Integer> first = new LinkedList<>();
+        Stack<Integer> second = new Stack<>();
+
+        int initSize = nums.size();
+        for(int i = 0; i < initSize; i++) {
+            if(initSize % 2 == 1 && i == initSize / 2) {
+                nums.poll();
+            }
+            else if(i < initSize / 2) {
+                first.offer(nums.poll());
+            }
+            else {
+                second.push(nums.poll());
+            }
+        }
+
+        while(!first.isEmpty()) if(first.poll() != second.pop()) return false;
+        return true;
+    }
+
+    Stack<Integer> sieveOfEratosthenes(int n) {
+        Queue<Integer> nums = new LinkedList<>();
+        Stack<Integer> primes = new Stack<>();
+
+        for(int i = 2; i <= n ; i++) nums.offer(i);
+
+        while(!nums.isEmpty()) {
+            int P = nums.poll();
+            primes.push(P);
+
+            Queue<Integer> temp = new LinkedList<>();
+            while(!nums.isEmpty()) {
+                int x = nums.poll();
+                if(x % P != 0) temp.offer(x);
+            }
+            nums = temp;
+        }
+
+        return primes;
     }
 }
--- src/Lab04_MelodyMaker/QueueProbsRunner.java
+++ src/Lab04_MelodyMaker/QueueProbsRunner.java
@@ -1,5 +1,27 @@
 package Lab04_MelodyMaker;
 
-public class QueueProbsRunner {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class QueueProbsRunner {
+    public static void main(String[] args) {
+        QueueProbs probs = new QueueProbs();
+
+        System.out.println(probs.evenFirst(arrayToQueue(new int[]{3, 5, 4, 17, 6, 83, 1, 84, 16, 37})));
+
+        System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 8, 17, 9, 17, 8, 3})));
+        System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 10, 17, 9, 17, 8 ,3})));
+        System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 8, 17, 17, 8, 3})));
+        System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 10, 17, 17, 8 ,3})));
+
+        System.out.println(probs.sieveOfEratosthenes(20));
+    }
+
+    public static Queue<Integer> arrayToQueue(int[] arr) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int j : arr) {
+            queue.offer(j);
+        }
+        return queue;
+    }
 }
