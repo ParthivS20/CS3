912278ed3994c71abd76833264cad9b8b23ef359
Parthiv Shah <parthivjs12@gmail.com>
2022-09-21 16:29:57 -0500

    finished maze solver

--- src/Lab04_MelodyMaker/QueueProbsRunner.java
+++ src/Lab04_MelodyMaker/QueueProbsRunner.java
@@ -7,13 +7,16 @@ public class QueueProbsRunner {
     public static void main(String[] args) {
         QueueProbs probs = new QueueProbs();
 
+        //evenFirst
         System.out.println(probs.evenFirst(arrayToQueue(new int[]{3, 5, 4, 17, 6, 83, 1, 84, 16, 37})));
 
+        //numPalindrome
         System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 8, 17, 9, 17, 8, 3})));
         System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 10, 17, 9, 17, 8 ,3})));
         System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 8, 17, 17, 8, 3})));
         System.out.println(probs.numPalindrome(arrayToQueue(new int[]{3, 10, 17, 17, 8 ,3})));
 
+        //sieveOfEratosthenes
         System.out.println(probs.sieveOfEratosthenes(20));
     }
 
