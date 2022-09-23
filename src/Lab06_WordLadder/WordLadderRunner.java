324a771971011396389c0b58998560ae0cc7d7ae
Parthiv Shah <parthivjs12@gmail.com>
2022-09-22 22:21:28 -0500

    finished maze solver

--- src/Lab06_WordLadder/WordLadderRunner.java
+++ src/Lab06_WordLadder/WordLadderRunner.java
@@ -1,5 +1,7 @@
 package Lab06_WordLadder;
 
+import Utils.ConsoleColors;
+
 import java.io.File;
 import java.util.*;
 
@@ -13,7 +15,9 @@ public class WordLadderRunner {
         Scanner dictFile =  new Scanner(new File("src/Lab06_WordLadder/dictionary.txt"));
         Scanner inputFile = new Scanner(new File("src/Lab06_WordLadder/input.txt"));
 
-        while(dictFile.hasNext()) dictionary.add(dictFile.nextLine().toLowerCase());
+        while(dictFile.hasNext()) {
+            dictionary.add(dictFile.nextLine().toLowerCase());
+        }
 
         while(inputFile.hasNext()) {
             String[] line = inputFile.nextLine().split(" ");
@@ -21,12 +25,12 @@ public class WordLadderRunner {
             String end = line[1];
 
             if(!dictionary.contains(start) || !dictionary.contains(end)) {
-                System.out.println("No Ladder found between " + start + " and " + end);
+                System.out.println("No Ladder found between " + color(start, ConsoleColors.GREEN) + " and " + color(end, ConsoleColors.CYAN));
                 continue;
             }
 
             if(start.equals(end)) {
-                System.out.println("Ladder Found! >>> [" + start + "]");
+                System.out.println("Ladder Found! >>> [" + color(start, ConsoleColors.GREEN) + "]");
                 continue;
             }
 
@@ -47,7 +51,30 @@ public class WordLadderRunner {
                 if(topWord.equals(end)) {
                     String ladder = "";
                     while(!topLadder.isEmpty()) {
-                        ladder = topLadder.pop() + ", " + ladder;
+                        String word = topLadder.pop();
+                        String text = word;
+                        if(topLadder.isEmpty()) {
+                            text = color(text, ConsoleColors.GREEN);
+                        }
+                        else {
+                            int changedIndex = -1;
+                            char[] word1 = word.toCharArray();
+                            char[] word2 = topLadder.peek().toCharArray();
+                            for(int i = 0; i < word1.length; i++) {
+                                if(word1[i] != word2[i]) {
+                                    changedIndex = i;
+                                    break;
+                                }
+                            }
+
+                            if(ladder.isBlank()) {
+                                text = color(text.substring(0, changedIndex), ConsoleColors.CYAN) + color(word1[changedIndex] + "",  ConsoleColors.CYAN_BOLD_BRIGHT) + color(text.substring(changedIndex + 1), ConsoleColors.CYAN);
+                            }
+                            else {
+                                text = text.substring(0, changedIndex) + color(word1[changedIndex] + "",  ConsoleColors.RED_BOLD) + text.substring(changedIndex + 1);
+                            }
+                        }
+                        ladder = text + ", " + ladder;
                     }
 
                     System.out.println("Ladder Found! >>> [" + ladder.substring(0, ladder.length() - 2) + "]");
@@ -77,13 +104,17 @@ public class WordLadderRunner {
             }
 
             if(!ladderFound) {
-                System.out.println("No Ladder found between " + start + " and " + end);
+                System.out.println("No Ladder found between " + color(start, ConsoleColors.GREEN) + " and " + color(end, ConsoleColors.CYAN));
             }
 
             dictionary.addAll(removedWords);
             removedWords.clear();
         }
 
-        System.out.println("Program Runtime: " + (System.nanoTime() - startTime) / 1000000 + "ms");
+        System.out.println("Program Runtime: " + color((System.nanoTime() - startTime) / 1000000 + "ms", ConsoleColors.BLUE));
+    }
+
+    public static String color(String text, String color) {
+        return color + text + ConsoleColors.RESET;
     }
 }
