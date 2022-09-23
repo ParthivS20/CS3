d513baf9ad26d901843658a9e43f8cb0ff89e300
Parthiv Shah <parthivjs12@gmail.com>
2022-09-22 21:15:37 -0500

    finished maze solver

--- src/Lab06_WordLadder/WordLadderRunner.java
+++ src/Lab06_WordLadder/WordLadderRunner.java
@@ -5,44 +5,79 @@ import java.util.*;
 
 public class WordLadderRunner {
     public static void main(String[] args) throws Exception {
-        HashSet<String> dict = new HashSet<>(), temp = new HashSet<>();
-        Scanner _s_dict =  new Scanner(new File("src/Lab06_WordLadder/dictionary.txt")), scan = new Scanner(new File("src/Lab06_WordLadder/input.txt"));
-        while(_s_dict.hasNext()) dict.add(_s_dict.nextLine().trim().toLowerCase());
-        while(scan.hasNext()) {
-            String[] line = scan.nextLine().split(" ");
-            String origin = line[0].trim(), fin = line[1].trim();
-            if(!dict.contains(origin)) System.out.println("No Ladder found between " + origin + " and " + fin);
-            else if(origin.equals(fin)) System.out.println("Ladder Found! (size 1) >>> [" + origin + "]");
-            else {
-                Queue<Stack<String>> bfs = new LinkedList<>();
-                Stack<String> _head = new Stack<>();
-                _head.push(origin); bfs.offer(_head); temp.add(origin); dict.remove(origin);
-                boolean found = false;
-                while(bfs.size() > 0) {
-                    Stack<String> top = bfs.poll();
-                    String prevWord = top.peek();
-                    if(prevWord.equalsIgnoreCase(fin)) {
-                        String ans = "]"; int sz = top.size();
-                        while(top.size() > 0) ans = top.pop() + (ans.length() > 1 ? ", "  : "") + ans;
-                        System.out.println("Ladder Found! (size " + sz + ") >>> [" + ans);
-                        found = true; break;
-                    } else {
-                        char[] word = prevWord.toCharArray();
-                        for(int i = 0; i < word.length; ++i)
-                            for(int j = 0; j < 26; ++j) {
-                                word[i] = (char)(((word[i]+1) - 97)%26 + 97);
-                                String ladder = String.valueOf(word);
-                                if(dict.contains(ladder)) {
-                                    dict.remove(ladder); temp.add(ladder);
-                                    Stack<String> copy = new Stack<String>();
-                                    copy.addAll(top); copy.push(ladder); bfs.offer(copy);
-                                }
+        HashSet<String> dictionary = new HashSet<>();
+        HashSet<String> removedWords = new HashSet<>();
+
+        Scanner dictFile =  new Scanner(new File("src/Lab06_WordLadder/dictionary.txt"));
+        Scanner inputFile = new Scanner(new File("src/Lab06_WordLadder/input.txt"));
+
+        while(dictFile.hasNext()) dictionary.add(dictFile.nextLine().toLowerCase());
+
+        int z = 0;
+        while(z < 1) {
+            String[] line = inputFile.nextLine().split(" ");
+            String start = line[0];
+            String end = line[1];
+
+            if(!dictionary.contains(start) || !dictionary.contains(end)) {
+                System.out.println("No Ladder found between " + start + " and " + end);
+                continue;
+            }
+
+            if(start.equals(end)) {
+                System.out.println("Ladder Found! >>> [" + start + "]");
+                continue;
+            }
+
+            Queue<Stack<String>> queue = new LinkedList<>();
+            Stack<String> stack = new Stack<>();
+
+            queue.offer(stack);
+            stack.push(start);
+
+            dictionary.remove(start);
+            removedWords.add(start);
+
+            boolean ladderFound = false;
+            while(!queue.isEmpty()) {
+                Stack<String> topLadder = queue.poll();
+                String topWord = topLadder.peek();
+
+                if(topWord.equals(end)) {
+                    String ladder = "";
+                    while(!topLadder.isEmpty()) ladder += topLadder.pop() + ", ";
+
+                    System.out.println("Ladder Found! >>> [" + ladder.substring(0, ladder.length() - 2) + "]");
+                    ladderFound = true;
+
+                    break;
+                } else {
+                    for(int i = 0; i < topWord.length(); i++) {
+                        char[] word = topWord.toCharArray();
+                        for(char j = 'a'; j <= 'z'; j++) {
+                            word[i] = j;
+                            String newWord = String.valueOf(word);
+
+                            if(dictionary.contains(newWord)) {
+                                dictionary.remove(newWord);
+                                removedWords.add(newWord);
+
+                                queue.offer(topLadder);
+                                topLadder.push(newWord);
+                                System.out.println(topLadder);
                             }
+                        }
                     }
                 }
-                if(!found) System.out.println("No Ladder found between " + origin + " and " + fin);
-                dict.addAll(temp); temp.clear();
             }
+
+            if(!ladderFound) {
+                System.out.println("No Ladder found between " + start + " and " + end);
+            }
+
+            dictionary.addAll(removedWords);
+            removedWords.clear();
+            z++;
         }
     }
 }
