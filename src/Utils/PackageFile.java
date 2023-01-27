7bef2d693f98de0099623343b19e0cc4f984fa2a
Parthiv Shah <parthivjs12@gmail.com>
2023-01-27 13:52:36 -0600

    Recurrent Refactor

--- src/Utils/PackageFile.java
+++ src/Utils/PackageFile.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public class PackageFile extends File {
     public PackageFile(String file, Class c) {
-        super("src/" + c.getPackage().toString().split(" ")[1] + "/" +file);
+        super("src/" + c.getPackage().toString().split(" ")[1] + "/" + file);
     }
 }
