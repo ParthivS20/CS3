5cae278e24e2b5732f291c74e67c2e083784339b
Parthiv Shah <parthivjs12@gmail.com>
2023-05-15 14:03:13 -0500

    Recurrent Refactor

--- src/Lab99_BonusLabs/Bitwise/Bitwise.java
+++ src/Lab99_BonusLabs/Bitwise/Bitwise.java
@@ -41,7 +41,7 @@ public class Bitwise {
     }
 
     static String q7_32BitBinaryString(int n) {
-        String binaryString = "";
+        var binaryString = "";
         for (int i = 0; i < 32; i++) {
             binaryString += q3_bitIs1(n << i, 31) ? "X" : " ";
         }
@@ -50,7 +50,7 @@ public class Bitwise {
     }
 
     static String q8_intToString(int n) {
-        String binaryString = "";
+        var binaryString = "";
         for (int i = 0; i < 32; i++) {
             if (i % 4 == 0) binaryString += " ";
             binaryString += n >>> (32 - i - 1) & 1;
@@ -60,7 +60,7 @@ public class Bitwise {
     }
 
     static String q9_intToHexString(int n) {
-        String hexString = "0x";
+        var hexString = "0x";
 
         for (int i = 0; i <= 7; i++) {
             hexString += HEX_SYMBOLS[n >>> (28 - i * 4) & 0xF];
@@ -69,23 +69,93 @@ public class Bitwise {
         return hexString;
     }
 
+    static void q10_binaryToInt() {
+        var binaryNums = new ArrayList<String>();
+        var inp = new Scanner(System.in);
+        while (inp.hasNextLine()) {
+            var bin = inp.nextLine().strip();
+            if (bin.isEmpty()) break;
+            if (bin.length() > 32) continue;
+
+            var invalid = false;
+            for (var c : bin.toCharArray()) {
+                if (c != '0' && c != '1') {
+                    invalid = true;
+                    break;
+                }
+            }
+
+            if (invalid) continue;
+
+            binaryNums.add(bin);
+        }
+
+        for (var binaryString : binaryNums) {
+            var i = 0;
+            for (var c : binaryString.toCharArray()) {
+                i = (i << 1) | c - '0';
+            }
+            System.out.println(binaryString + " => " + i);
+        }
+    }
+
+    static void q11_hexToInt() {
+        var hexNums = new ArrayList<String>();
+        var inp = new Scanner(System.in);
+        while (inp.hasNextLine()) {
+            var hex = inp.nextLine().strip().toUpperCase();
+            if (hex.isEmpty()) break;
+
+            if (hex.startsWith("0x")) hex = hex.substring(2);
+            if (hex.length() > 8) continue;
+
+            var invalid = false;
+            for (var c : hex.toCharArray()) {
+                if ((c < '0' || c > '9') && (c < 'A' || c > 'F')) {
+                    invalid = true;
+                    break;
+                }
+            }
+
+            if (invalid) continue;
+
+            hexNums.add(hex);
+        }
+
+        for (var hexString : hexNums) {
+            var i = 0;
+            for (var c : hexString.toCharArray()) {
+                i = (i << 4) | c - (c >= '0' && c <= '9' ? '0' : 'A' - 10);
+            }
+            System.out.println("0x" + hexString + " => " + i);
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner file = new Scanner(new PackageFile("data/numbers.txt", Bitwise.class));
         ArrayList<Integer> nums = new ArrayList<>();
         while (file.hasNextInt()) nums.add(file.nextInt());
 
-        /*
+
         System.out.println(q1_isEven(12));
         System.out.println(q1_isEven(15));
 
+
+        /*
         System.out.println(q2_isPowerOf2(6));
         System.out.println(q2_isPowerOf2(16));
+        */
 
+        /*
         System.out.println(q3_bitIs1(10, 1));
         System.out.println(q3_bitIs1(10, 3));
+        */
 
+        /*
         System.out.println(q4_setBitTo1(0, 0));
+        */
 
+        /*
         System.out.println(q5_toggleBit(1, 0));
         */
 
@@ -93,13 +163,20 @@ public class Bitwise {
         for(int n: nums) {
             System.out.println(q7_32BitBinaryString(n));
         }
+        */
 
+        /*
         System.out.println(q8_intToString(42));
         System.out.println(q8_intToString(-1001));
         */
 
+        /*
         for (int n : nums) {
             System.out.println(q9_intToHexString(n));
         }
+         */
+
+        //q10_binaryToInt();
+        q11_hexToInt();
     }
 }
